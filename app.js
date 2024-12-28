fe02ae85b94b132b504ba43e85fcd8be106d3283
YoannPohu2 <153502409+YoannPohu2@users.noreply.github.com>
2024-12-28 19:57:04 +0100

    Ajout de l'affichage du type de Pokémon

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let button = document.getElementById('button');
 let image = document.getElementById('image');
 let pokeNumber = document.getElementById('number');
 let pokeName = document.getElementById('name');
+let pokeType = document.getElementById('type'); // Ajout d'un élément pour le type
 
 const changePokemon = async () => {
     let randomNumber = Math.ceil(Math.random() * 150) + 1; // 1 et 151
@@ -16,6 +17,7 @@ const changePokemon = async () => {
     image.src = response.sprites.front_default;
     pokeNumber.textContent = `${response.id}`;
     pokeName.textContent = response.name;
+    pokeType.textContent = response.types.map(typeInfo => typeInfo.type.name).join(', '); // Affichage du type
 }
 
 changePokemon();
